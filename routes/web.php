1d57a79b4028f1101e9fa0244f922a6bedc64308
yohanlaborda <yohan.laborda@gmail.com>
2024-06-17 23:17:47 +0200

    feat: define the url the endpoint of the short url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShortUrlsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::post('/api/v1/short-urls', ShortUrlsController::class);
