09b6f305a4f68df4545e087c2128078bd9987bd7
yohanlaborda <yohan.laborda@gmail.com>
2024-06-17 23:16:55 +0200

    chore: defining the dependency injection for services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,39 @@
 
 namespace App\Providers;
 
+use GuzzleHttp\Client;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Url\Application\ShortUrlGenerator;
+use Url\Domain\UrlProvider;
+use Url\Infrastructure\TinyUrl\TinyUrlProvider;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            TinyUrlProvider::class,
+            static function (Application $application) {
+                return new TinyUrlProvider(
+                    $application->make(Client::class),
+                    config('app.urlProvider.tinyUrl')
+                );
+            }
+        );
+
+        $this->app->when(ShortUrlGenerator::class)
+            ->needs(UrlProvider::class)
+            ->give(
+                static function (Application $application) {
+                    return [
+                        $application->make(TinyUrlProvider::class),
+                    ];
+                }
+            );
     }
 
     /**
